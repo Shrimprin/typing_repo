49bd4738b007179f1f13d55f25e736c7c5a0fa41
Kuroneko <ken.aster8@gmail.com>
2025-04-06 20:15:18 +0900

    eslintをセットアップ

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,6 +1,12 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+import eslint from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+
+import eslintPluginReadableTailwind from 'eslint-plugin-readable-tailwind';
+import tseslint from 'typescript-eslint';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +15,32 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default tseslint.config(
+  {
+    files: ['*.ts', '*.tsx'],
+  },
+  {
+    ignores: ['**/.next/**/*'],
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    files: ['**/*.{jsx,tsx}'],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      'readable-tailwind': eslintPluginReadableTailwind,
+    },
+    rules: {
+      ...eslintPluginReadableTailwind.configs.warning.rules,
+      ...eslintPluginReadableTailwind.configs.error.rules,
+    },
+  },
+  eslintConfigPrettier,
+);
