83e11f9ba2ef21ee20153bccb74537287a8f3140
Kuroneko <ken.aster8@gmail.com>
2025-04-27 11:02:49 +0900

    createの異常系のテストを作成

--- backend/spec/requests/api/repositories_spec.rb
+++ backend/spec/requests/api/repositories_spec.rb
@@ -17,12 +17,12 @@ RSpec.describe 'Api::Repositories', type: :request do
     end
 
     before do
+      create(:user)
       allow(ENV).to receive(:fetch).with('GITHUB_ACCESS_TOKEN').and_return('github_access_token')
     end
 
     context 'when request is valid' do
       before do
-        create(:user)
         github_client_mock = instance_double(Octokit::Client)
         allow(Octokit::Client).to receive(:new).and_return(github_client_mock)
         allow(github_client_mock).to receive(:repository).with(valid_repository_url).and_return(repository_info)
@@ -37,9 +37,80 @@ RSpec.describe 'Api::Repositories', type: :request do
         post api_repositories_path, params: { repository: { url: valid_url } }, headers: headers
         expect(response).to have_http_status(:created)
         json = response.parsed_body
-
         expect(json.length).to eq 1
         expect(json['repository']['name']).to eq('repository')
+      end
+    end
+
+    context 'when url is invalid' do
+      it 'returns unprocessable_entity status' do
+        invalid_url = 'https://invalid_url.com'
+        post api_repositories_path, params: { repository: { url: invalid_url } }, headers: headers
+        expect(response).to have_http_status(:unprocessable_entity)
+        json = response.parsed_body
+        expect(json['error']).to eq('無効なURLです。')
+      end
+    end
+
+    context 'when repository is non-existent' do
+      before do
+        non_existent_repository_url = 'username/invalid_url'
+        github_client_mock = instance_double(Octokit::Client)
+        allow(Octokit::Client).to receive(:new).and_return(github_client_mock)
+        allow(github_client_mock).to receive(:repository).with(non_existent_repository_url).and_raise(Octokit::NotFound)
+      end
+
+      it 'returns not found status' do
+        non_existent_url = 'https://github.com/username/invalid_url'
+        post api_repositories_path, params: { repository: { url: non_existent_url } }, headers: headers
+        expect(response).to have_http_status(:not_found)
+        json = response.parsed_body
+        expect(json['error']).to eq('リポジトリが存在しません。')
+      end
+    end
+
+    context 'when too many requests' do
+      before do
+        github_client_mock = instance_double(Octokit::Client)
+        allow(Octokit::Client).to receive(:new).and_return(github_client_mock)
+        allow(github_client_mock).to receive(:repository).with(valid_repository_url).and_raise(Octokit::TooManyRequests)
+      end
+
+      it 'returns too_many_requests status' do
+        post api_repositories_path, params: { repository: { url: valid_url } }, headers: headers
+        expect(response).to have_http_status(:too_many_requests)
+        json = response.parsed_body
+        expect(json['error']).to eq('APIリクエストが多すぎます。少し時間をおいてから再度実行してください。')
+      end
+    end
+
+    context 'when unauthorized' do
+      before do
+        github_client_mock = instance_double(Octokit::Client)
+        allow(Octokit::Client).to receive(:new).and_return(github_client_mock)
+        allow(github_client_mock).to receive(:repository).with(valid_repository_url).and_raise(Octokit::Unauthorized)
+      end
+
+      it 'returns unauthorized status' do
+        post api_repositories_path, params: { repository: { url: valid_url } }, headers: headers
+        expect(response).to have_http_status(:unauthorized)
+        json = response.parsed_body
+        expect(json['error']).to eq('アクセストークンが無効です。')
+      end
+    end
+
+    context 'when unexpected error occurs' do
+      before do
+        github_client_mock = instance_double(Octokit::Client)
+        allow(Octokit::Client).to receive(:new).and_return(github_client_mock)
+        allow(github_client_mock).to receive(:repository).with(valid_repository_url).and_raise(StandardError)
+      end
+
+      it 'returns internal_server_error status' do
+        post api_repositories_path, params: { repository: { url: valid_url } }, headers: headers
+        expect(response).to have_http_status(:internal_server_error)
+        json = response.parsed_body
+        expect(json['error']).to eq('リポジトリ情報の取得中にエラーが発生しました。')
       end
     end
   end
