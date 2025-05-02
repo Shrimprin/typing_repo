128dae4fd4717bcf1d5b79243c42ad456d8c3590
Kuroneko <ken.aster8@gmail.com>
2025-05-02 13:30:18 +0900

    バックエンドのログイン処理を作成

---
+++ backend/app/controllers/api/auth_controller.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Api
+  class AuthController < ApplicationController
+    skip_before_action :authenticate_request!
+
+    def login
+      user = User.find_or_create_by!(login_params)
+      access_token = JsonWebToken.encode(user_id: user.id)
+      Rails.logger.debug access_token
+      if user
+        render json: { access_token: access_token }, status: :ok
+      else
+        render json: { error: 'ログインに失敗しました' }, status: unprocessable_entity
+      end
+    rescue ActiveRecord::RecordInvalid => e
+      render json: { error: e.message }, status: :unprocessable_entity
+    rescue StandardError => e
+      render json: { error: e.message }, status: :internal_server_error
+    end
+
+    private
+
+    def login_params
+      params.permit(:github_id, :name)
+    end
+  end
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  post '/api/auth/callback/github', to: 'api/auth#login'
+
   namespace :api do
     resources :repositories, only: [:create]
   end
