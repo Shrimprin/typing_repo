3580bd8a6b84659ff972c5ec5073b0ae54dbc963
Kuroneko <ken.aster8@gmail.com>
2025-05-02 15:44:24 +0900

    不要なifを削除

--- backend/app/controllers/api/auth_controller.rb
+++ backend/app/controllers/api/auth_controller.rb
@@ -7,11 +7,7 @@ module Api
     def login
       user = User.find_or_create_by!(login_params)
       access_token = JsonWebToken.encode(user_id: user.id)
-      if user
-        render json: { access_token: access_token }, status: :ok
-      else
-        render json: { error: 'ログインに失敗しました' }, status: unprocessable_entity
-      end
+      render json: { access_token: access_token }, status: :ok
     rescue ActiveRecord::RecordInvalid => e
       render json: { error: e.message }, status: :unprocessable_entity
     rescue StandardError => e
