610cb70b441ad7b3711407e4992ca672f011c5ef
Kuroneko <ken.aster8@gmail.com>
2025-05-14 15:43:31 +0900

    repositoryのindexアクションを作成

--- backend/app/serializers/repository_serializer.rb
+++ backend/app/serializers/repository_serializer.rb
@@ -6,4 +6,14 @@ class RepositorySerializer
   root_key :repository, :repositories
 
   attributes :id, :user_id, :name, :last_typed_at
+
+  attribute :progress, if: proc { params[:progress] } do |repository|
+    files = repository.file_items.where(type: :file)
+    if files.empty?
+      100.0
+    else
+      typed_count = files.where(status: :typed).count
+      typed_count.to_f / files.count
+    end
+  end
 end
