4ded97560589c1c20939300613d49cbefb62290d
Kuroneko <ken.aster8@gmail.com>
2025-06-11 22:05:34 +0900

    file_itemのupdateアクションを作成（タイピング完了処理用）

--- backend/app/controllers/api/file_items_controller.rb
+++ backend/app/controllers/api/file_items_controller.rb
@@ -9,5 +9,22 @@ module Api
     rescue ActiveRecord::RecordNotFound
       render json: { error: 'ファイルが存在しません。' }, status: :not_found
     end
+
+    def update
+      repository = @current_user.repositories.find(params[:repository_id])
+      file_item = repository.file_items.find(params[:id])
+      if file_item.update_with_parent(file_item_params)
+        top_level_file_items = repository.file_items.roots
+        render json: FileItemSerializer.new(top_level_file_items, params: { children: true }), status: :ok
+      else
+        render json: { error: 'ファイルの更新に失敗しました。' }, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def file_item_params
+      params.expect(file_item: [:status])
+    end
   end
 end
