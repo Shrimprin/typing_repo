b655ca3736940b152aba04f528516339a033cd60
Kuroneko <ken.aster8@gmail.com>
2025-06-18 22:09:20 +0900

    コンポーネント名を明記

--- frontend/src/app/repositories/new/page.tsx
+++ frontend/src/app/repositories/new/page.tsx
@@ -1,13 +1,13 @@
 import { NextPage } from 'next';
 
-import Form from '@/components/repositories/RepositoryForm';
+import RepositoryForm from '@/components/repositories/RepositoryForm';
 
 const NewRepositoryPage: NextPage = () => {
   return (
     <div className="mx-auto max-w-md p-4">
       <h1 className="mb-4 text-xl font-bold">リポジトリを追加</h1>
       <p className="mb-4 text-sm text-gray-500">タイピングしたいGitHubリポジトリのURLを入力してください。</p>
-      <Form />
+      <RepositoryForm />
     </div>
   );
 };
