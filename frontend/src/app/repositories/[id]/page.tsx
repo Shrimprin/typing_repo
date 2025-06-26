163a40fb0b8075d9e8db856a51fbe7ed233395d4
Kuroneko <ken.aster8@gmail.com>
2025-06-27 07:22:23 +0900

    ヘッダーを作成

--- frontend/src/app/repositories/[id]/page.tsx
+++ frontend/src/app/repositories/[id]/page.tsx
@@ -1,7 +1,10 @@
 import { AxiosError } from 'axios';
+import { Settings, Trash2 } from 'lucide-react';
 
 import { auth } from '@/auth';
+import Header from '@/components/common/Header';
 import RepositoryDetail from '@/components/repositories/RepositoryDetail';
+import { DropdownMenuItem } from '@/components/ui/dropdown-menu';
 import { FileItem, Repository } from '@/types';
 import { fetcher } from '@/utils/fetcher';
 import { sortFileItems } from '@/utils/sort';
@@ -27,5 +30,23 @@ export default async function RepositoryDetailPage({ params }: RepositoryDetailP
   const fileItems: FileItem[] | [] = repository.fileItems;
   const sortedFileItems = sortFileItems(fileItems);
 
-  return <RepositoryDetail initialFileItems={sortedFileItems} />;
+  const moreComponent = (
+    <>
+      <DropdownMenuItem>
+        <Settings className="mr-2 h-4 w-4 font-mono" />
+        設定
+      </DropdownMenuItem>
+      <DropdownMenuItem className="font-mono text-red-600">
+        <Trash2 className="mr-2 h-4 w-4 text-red-600" />
+        リポジトリを削除
+      </DropdownMenuItem>
+    </>
+  );
+
+  return (
+    <>
+      <Header title={repository.name} moreComponent={moreComponent} />
+      <RepositoryDetail initialFileItems={sortedFileItems} />
+    </>
+  );
 }
--- frontend/src/app/repositories/new/page.tsx
+++ frontend/src/app/repositories/new/page.tsx
@@ -1,14 +1,18 @@
 import { NextPage } from 'next';
 
+import Header from '@/components/common/Header';
 import RepositoryForm from '@/components/repositories/RepositoryForm';
 
 const NewRepositoryPage: NextPage = () => {
   return (
-    <div className="mx-auto max-w-md p-4">
-      <h1 className="mb-4 text-xl font-bold">リポジトリを追加</h1>
-      <p className="mb-4 text-sm text-gray-500">タイピングしたいGitHubリポジトリのURLを入力してください。</p>
-      <RepositoryForm />
-    </div>
+    <>
+      <Header title="リポジトリを追加" />
+      <div className="mx-auto max-w-md p-4">
+        <h1 className="mb-4 text-xl font-bold">リポジトリを追加</h1>
+        <p className="mb-4 text-sm text-gray-500">タイピングしたいGitHubリポジトリのURLを入力してください。</p>
+        <RepositoryForm />
+      </div>
+    </>
   );
 };
 
