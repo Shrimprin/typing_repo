43d80513f297d8f7b06156d11fd64f3a90c4a1fc
Kuroneko <ken.aster8@gmail.com>
2025-07-30 14:14:10 +0900

    extensionモデルとルーティングを作成

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   post '/api/auth/callback/github', to: 'api/auth#login'
 
   namespace :api do
+    resources :repository_previews, only: [:create]
     resources :repositories, only: [:index, :show, :create] do
       resources :file_items, only: [:show, :update]
+      member do
+        get :extensions
+      end
     end
   end
 end
