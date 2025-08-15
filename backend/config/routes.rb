69fed96bbaffb6837af6ef1315b0d7429f122160
Kuroneko <ken.aster8@gmail.com>
2025-08-15 15:39:27 +0900

    記述を簡略化

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,9 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     resources :repositories, only: [:index, :show, :create] do
       resources :file_items, only: [:show, :update]
-      collection do
-        get :preview
-      end
+      get :preview, on: :collection
     end
   end
 end
