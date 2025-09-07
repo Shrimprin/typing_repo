99826cef4eba4f4fcfbd4cc78b719c454addbc9e
Kuroneko <ken.aster8@gmail.com>
2025-09-07 14:24:43 +0900

    ユーザーidは返す必要が無いため削除

--- backend/app/serializers/repository_serializer.rb
+++ backend/app/serializers/repository_serializer.rb
@@ -3,7 +3,7 @@
 class RepositorySerializer
   include Alba::Serializer
 
-  attributes :id, :last_typed_at, :name, :user_id
+  attributes :id, :last_typed_at, :name
 
   attribute :file_items, if: proc { params[:file_items] } do |repository|
     file_items_grouped_by_parent = repository.file_items_grouped_by_parent
@@ -12,5 +12,8 @@ class RepositorySerializer
     FileItemSerializer.new(top_level_file_items, params: { children: true, file_items_grouped_by_parent: })
   end
 
-  attribute :progress, if: proc { params[:progress] }, &:progress
+  # attribute :progress, if: proc { params[:progress] }, &:progress
+  attribute :progress do |repository|
+    1
+  end
 end
