15bd69eb3671b09b56ce08c0fa833b47383904db
Kuroneko <ken.aster8@gmail.com>
2025-09-17 17:32:35 +0900

    バックエンドのテストを作成

--- backend/spec/factories/typing_progress.rb
+++ backend/spec/factories/typing_progress.rb
@@ -8,5 +8,11 @@ FactoryBot.define do
     elapsed_seconds { 0 }
     total_correct_type_count { 0 }
     total_typo_count { 0 }
+
+    trait :with_typos do
+      after(:create) do |typing_progress|
+        create_list(:typo, 2, typing_progress:)
+      end
+    end
   end
 end
