cabe83751f140500238a3bde924be6f9c0a441c7
Kuroneko <ken.aster8@gmail.com>
2025-09-27 10:44:55 +0900

    色を修正

--- frontend/src/components/ui/sonner.tsx
+++ frontend/src/components/ui/sonner.tsx
@@ -11,11 +11,28 @@ const Toaster = ({ ...props }: ToasterProps) => {
       theme={theme as ToasterProps['theme']}
       className="toaster group"
       position="top-center"
+      richColors={true}
       style={
         {
           '--normal-bg': 'var(--popover)',
           '--normal-text': 'var(--popover-foreground)',
           '--normal-border': 'var(--border)',
+
+          '--success-bg': 'var(--secondary-background)',
+          '--success-border': 'var(--secondary)',
+          '--success-text': 'var(--secondary)',
+
+          '--info-bg': 'var(--primary-background)',
+          '--info-border': 'var(--primary)',
+          '--info-text': 'var(--primary)',
+
+          '--warning-bg': 'var(--warning-background)',
+          '--warning-border': 'var(--warning)',
+          '--warning-text': 'var(--warning)',
+
+          '--error-bg': 'var(--destructive-background)',
+          '--error-border': 'var(--destructive)',
+          '--error-text': 'var(--destructive)',
         } as React.CSSProperties
       }
       {...props}
