b8e370fe1648df2f9a363c52b58f21a0b1f928c7
Kuroneko <ken.aster8@gmail.com>
2025-10-17 13:12:11 +0900

    OGPとXカードを設定

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,11 +1,46 @@
+import { Metadata } from 'next';
 import { Play } from 'next/font/google';
 
 import Footer from '@/components/common/Footer';
 import ToastListener from '@/components/common/ToastListener';
 import { Toaster } from '@/components/ui/sonner';
 import AuthProvider from '@/providers/Auth';
-
 import './globals.css';
+
+const TITLE = 'Typing Repo';
+const DESCRIPTION =
+  'Free typing game for programmers. You can import any GitHub repository and practice typing through real code.';
+const URL = 'https://typing-repo.com';
+
+export const metadata: Metadata = {
+  title: {
+    default: `${TITLE} | Practice Typing through Real Code`,
+    template: '%s | Typing Repo',
+  },
+  description: DESCRIPTION,
+  openGraph: {
+    title: TITLE,
+    description: DESCRIPTION,
+    url: URL,
+    images: [
+      {
+        url: '/ogp.png',
+        alt: 'Typing Repo',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: TITLE,
+    description: DESCRIPTION,
+    images: [
+      {
+        url: '/ogp.png',
+        alt: 'Typing Repo',
+      },
+    ],
+  },
+};
 
 const play = Play({
   variable: '--font-play',
